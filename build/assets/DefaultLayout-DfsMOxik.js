182c3903b57d3975ccbe54aafdcab5fb097211c5
Kashish <kashish.biserwal@protonmail.com>
2024-07-03 17:30:12 +0530

    add build

---
+++ build/assets/DefaultLayout-DfsMOxik.js
@@ -0,0 +1,1 @@
+import{j as s,A as e,a,b as r}from"./index-B8WALBmV.js";const d=()=>s.jsxs("div",{children:[s.jsx(e,{}),s.jsxs("div",{className:"wrapper d-flex flex-column min-vh-100",children:[s.jsx(a,{}),s.jsx("div",{className:"body flex-grow-1",children:s.jsx(r,{})})]})]});export{d as default};
